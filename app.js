73948b8405ed31440e8fb7c004e8735c531417df
naeemtalukder <naeemtalukder169135@gmail.com>
2023-03-04 23:46:23 +0600

    get element by qurey-selector

---
+++ app.js
@@ -0,0 +1,9 @@
+const loanAmountInput = document.querySelector(".loan-amount");
+const interestRateInput = document.querySelector(".interest-rate");
+const loanTenureInput = document.querySelector(".loan-tenure");
+
+const loanEMIValue = document.querySelector(".loan-emi")
+const totalInterestValue = document.querySelector(".total-interest")
+const totalAmountValue = document.querySelector(".total-amount")
+
+const calculateBtn = document.querySelector(".calculate-btn")
