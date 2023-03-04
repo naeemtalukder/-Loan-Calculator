ba7d6db354ddfdeed7b096963a85fe75a246afa6
naeemtalukder <naeemtalukder169135@gmail.com>
2023-03-04 23:54:26 +0600

    calculated function almost doen

--- app.js
+++ app.js
@@ -2,8 +2,25 @@ const loanAmountInput = document.querySelector(".loan-amount");
 const interestRateInput = document.querySelector(".interest-rate");
 const loanTenureInput = document.querySelector(".loan-tenure");
 
-const loanEMIValue = document.querySelector(".loan-emi")
-const totalInterestValue = document.querySelector(".total-interest")
-const totalAmountValue = document.querySelector(".total-amount")
+const loanEMIValue = document.querySelector(".loan-emi");
+const totalInterestValue = document.querySelector(".total-interest");
+const totalAmountValue = document.querySelector(".total-amount");
 
-const calculateBtn = document.querySelector(".calculate-btn")
+const calculateBtn = document.querySelector(".calculate-btn");
+
+
+let loanAmount = parseFloat(loanAmountInput.value);
+let interestRate = parseFloat(interestRateInput.value);
+loanTenure = parseFloat(loanTenureInput.value);
+
+let interest = interestRate / 12 / 100;
+
+const calculateEMI = () => {
+    let emi = 
+    loanAmount *
+    interest * 
+    (Math.pow(1 + interest, loanTenure) / 
+    (Math.pow(1 + interest, loanTenure) -1));
+
+    return emi;
+}
